cd0bf5bdd73f95f70fcea60b81116059b71e189d
gn0m30 <reggie@loco8.org>
2011-01-30 19:31:53 -0500

    adding sites

--- servers/tiny_ruby/configuration.rb
+++ servers/tiny_ruby/configuration.rb
@@ -3,8 +3,13 @@
 #
 require File.join(File.expand_path(File.dirname(__FILE__)), "..", "common_configuration")
 
+#  /opt/nginx/conf/sites-available/ctrain
+#  /opt/nginx/conf/sites-available/sassywood
+#  /opt/nginx/conf/sites-available/bankofbs
+
 CONFIG_FILES = %w(
   /opt/nginx/conf/nginx.conf
+  /opt/nginx/conf/sites-available/default
 )
 
 module Configuration
@@ -13,6 +18,14 @@ module Configuration
 
   # commands to reset a server to a current
   def reload
+    # makes sure the sites are enabled
+    CONFIG_FILES.each do |cfg|
+      if cfg.include?('sites-available')
+        link = cfg.gsub('sites-available', 'sites-enabled')
+        FileUtils.ln_sf  cfg, link unless File.exists?(link)
+      end
+    end
+
     system('/etc/init.d/nginx reload')
   end
 
